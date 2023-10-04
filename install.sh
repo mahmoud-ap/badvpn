d1440cbfa8661d293075f46d75f748202cc89b0b
Mahmoud <mahmoud.farhadi91@gmail.com>
2023-10-05 01:42:32 +0330

    first added

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-OS=`uname -m`;
 sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/mahmoud-ap/badvpn/master/badvpn-udpgw"
-if [ "$OS" == "x86_64" ]; then
-  sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/mahmoud-ap/badvpn/master/badvpn-udpgw64"
-fi
 sudo touch /etc/rc.local
-sudo echo "\nscreen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300" >> /etc/rc.local
+sudo nano /etc/rc.local
+
+cat >  /etc/rc.local << ENDOFFILE
+#!/bin/sh -e
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+exit 0
+ENDOFFILE
+
+chmod +x /etc/rc.local
+sudo systemctl status rc-local.service
 sudo chmod +x /usr/bin/badvpn-udpgw
 sudo screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
