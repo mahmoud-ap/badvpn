11f54c46f2a27d6b752c265408bcbfa14e077005
Mahmoud <141625684+mahmoud-ap@users.noreply.github.com>
2023-10-29 11:40:20 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-username="useradd -m videocall"
-if id "$username" &>/dev/null; then
+local username="videocall"
+
+if grep -q "^$username:" /etc/passwd; then
     echo "videocall is installed."
 else
-    sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/mahmoud-ap/badvpn/master/badvpn-udpgw"
-    mv -f ./badvpn-udpgw /usr/bin/badvpn-udpgw
+    local vendorId=$(lscpu | awk -F': ' '/Vendor ID/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}')
+    if [ "$vendorId" = "ARM" ]; then
+        sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/mahmoud-ap/badvpn/master/udpgw-arm"
+    else
+        sudo wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/mahmoud-ap/badvpn/master/udpgw-x86"
+    fi
     chmod 777 /usr/bin/badvpn-udpgw
     useradd -m videocall
 fi
@@ -16,7 +21,7 @@ Description=UDP forwarding for badvpn-tun2socks
 After=nss-lookup.target
 
 [Service]
-ExecStart=/usr/bin/badvpn-udpgw --loglevel none --listen-addr 127.0.0.1:7301 --max-clients 999
+ExecStart=/usr/bin/badvpn-udpgw --loglevel none --listen-addr 127.0.0.1:7302 --max-clients 999
 User=videocall
 
 [Install]
@@ -25,4 +30,5 @@ ENDOFFILE
 
 systemctl enable videocall
 systemctl start videocall
-   
+
+echo "videocall configured"
